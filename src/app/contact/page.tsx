f0e7c963bba58185628cc843ebdf05df39d36507
Warda Waseem <160673839+WardaWaseem2812@users.noreply.github.com>
2024-10-10 03:05:02 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ const ContactPage = () => {
     return (
       <div>
         <h1 className={cssstyle.contact}>This is ContactPage</h1>
-        <p>Contact us at assignment@gmail.com</p>
+        <p>Please contact us at wwk@gmail.com</p>
       </div>
     )
   }
